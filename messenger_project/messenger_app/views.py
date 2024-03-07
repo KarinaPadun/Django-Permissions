c9180d7ffa9b04d9e03966cb49bd3cc02dec3fa4
karina <kmalina946@gmail.com>
2024-03-07 14:21:59 +0200

    editing

--- messenger_project/messenger_app/views.py
+++ messenger_project/messenger_app/views.py
@@ -1,45 +1,40 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import DetailView, UpdateView, CreateView
+
+from .models import Chat, Message
+from .mixins import LoginRequiredMixin, ChatPermissionMixin, MessagePermissionMixin, FormValidMixin
 
 @login_required
-def chat_detail(request, chat_id):
-    chat = get_object_or_404(Chat, pk=chat_id)
+def chat_list(request):
+    chats = Chat.objects.filter(users=request.user)
+    return render(request, 'messenger_app/chat_list.html', {'chats': chats})
 
-    if not chat.has_permission(request.user):
-        messages.error(request, "У вас нет доступа к этому чату.")
-        return redirect('home')
+class ChatDetailView(LoginRequiredMixin, ChatPermissionMixin, DetailView):
+    model = Chat
 
-    messages = Message.objects.filter(chat=chat).order_by('timestamp')
-    form = MessageForm()
+class EditMessageView(LoginRequiredMixin, MessagePermissionMixin, UpdateView):
+    model = Message
+    fields = ['content']
 
-    if request.method == 'POST':
-        form = MessageForm(request.POST)
-        if form.is_valid():
-            message = form.save(commit=False)
-            message.author = request.user
-            message.chat = chat
-            message.save()
-            messages.success(request, 'Сообщение отправлено.')
-            form = MessageForm()
+class CreateMessageView(LoginRequiredMixin, ChatPermissionMixin, FormValidMixin, CreateView):
+    model = Message
+    fields = ['content']
 
-    return render(request, 'messenger_app/chat_detail.html', {'chat': chat, 'messages': messages, 'form': form})
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.chat = Chat.objects.get(pk=self.kwargs['chat_id'])
+        return super(CreateMessageView, self).form_valid(form)
 
 @login_required
-def edit_message(request, message_id):
+def delete_message(request, message_id):
     message = get_object_or_404(Message, pk=message_id)
 
     if not message.has_permission(request.user):
         messages.error(request, "У вас нет доступа к этому сообщению.")
         return redirect('home')
 
-    if request.method == 'POST':
-        form = MessageForm(request.POST, instance=message)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Сообщение отредактировано.')
-            return redirect('messenger_app:chat_detail', chat_id=message.chat.id)
-
-    form = MessageForm(instance=message)
-    return render(request, 'messenger_app/edit_message.html', {'form': form, 'message': message})
-
+    message.delete()
+    messages.success(request, "Сообщение удалено.")
+    return redirect('messenger_app:chat_detail', chat_id=message.chat.id)
