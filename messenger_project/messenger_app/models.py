8b20a2c5a2e2fa6d87631ddf6b311adbfbb71048
karina <kmalina946@gmail.com>
2024-03-20 00:47:42 +0200

    editing

--- messenger_project/messenger_app/models.py
+++ messenger_project/messenger_app/models.py
@@ -2,6 +2,15 @@ from django.contrib.auth.models import User, Group
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 
+class File(models.Model):
+    name = models.CharField(max_length=255)
+    file = models.FileField(upload_to='uploads/')
+    content_type = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
 class Chat(models.Model):
     name = models.CharField(max_length=255)
     users = models.ManyToManyField(User, related_name='chats')
