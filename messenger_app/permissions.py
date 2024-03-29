713d2dbb144dfed356bf91184251c0414f41ae53
karina <kmalina946@gmail.com>
2024-03-29 22:49:13 +0200

    editing

--- messenger_app/permissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import permissions
-
-class IsChatUserOrSuperuser(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return obj.author == request.user or request.user.is_superuser
-
-class IsChatSuperuser(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_superuser
