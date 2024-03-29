776ef3f8ea59d25ebea205177b8d59a80d36e7e6
karina <kmalina946@gmail.com>
2024-03-29 22:43:03 +0200

    editing

--- messenger_project/messenger_project/settings.py
+++ messenger_project/messenger_project/settings.py
@@ -40,6 +40,12 @@ INSTALLED_APPS = [
     'messenger_app',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
